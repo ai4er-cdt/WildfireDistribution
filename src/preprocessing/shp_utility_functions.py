b6d4def79b130c0b8ff2d25ae3e663fb1565a7bb
Hamish Campbell <hrac2@cam.ac.uk>
2021-12-17 12:32:11 +0000

    Added vertices extraction function to shp utilities file. Also implemented functions in the notebook for modis data.

--- src/preprocessing/shp_utility_functions.py
+++ src/preprocessing/shp_utility_functions.py
@@ -1,3 +1,19 @@
+import geopandas as gpd
+import numpy as np
+import rioxarray as rxr
+
+def extract_vertices(shape_file_path):
+    """Returns a numpy array of the lat/lons corresponding to the vertices of a polygon defined by a shape file.
+    
+    Keyword arguments:
+    shape_file_path -- realtive path where the shp and shx files are located (both required)
+    """
+    
+    polygons_df = gpd.read_file(shape_file_path)
+    vertices = list([list(coords) for coords in polygons_df.iloc[0]['geometry'].exterior.coords])
+    return np.array(vertices)
+    
+
 def crop_data_spatially(input_data, shape_file_path, zero_remap = 0):
     """Crops input data to keep only the elements within the spatial bounds defined by the input shape file.
     
@@ -10,17 +26,16 @@ def crop_data_spatially(input_data, shape_file_path, zero_remap = 0):
     
     # Check that zero_remap is the same type as the values in input_data
     try:
-        if not ((isinstance(zero_remap, (int, np.integer)) and isinstance(burn_days.values[0,0,0], (int, np.integer))) or
-                 (isinstance(zero_remap, (float, np.float64)) and isinstance(burn_days.values[0,0,0], (float, np.float64)))):
-            raise TypeError('The zero_remap value provided must match the type of the input data:', type(zero_remap), type(burn_days.values.dtype)) 
+        if not ((isinstance(zero_remap, (int, np.integer)) and isinstance(input_data.values[0,0,0], (int, np.integer))) or
+                 (isinstance(zero_remap, (float, np.float64)) and isinstance(input_data.values[0,0,0], (float, np.float64)))):
+            raise TypeError('The zero_remap value provided must match the type of the input data:', type(zero_remap), type(input_data.values.dtype)) 
     
     except TypeError as error:
         print(error.args)
     
     
     # Extract the vertice's latitude/longitudes from the shape file
-    polygons_df = gpd.read_file(shape_file_path)
-    vertices = list([list(coords) for coords in polygons_df.iloc[0]['geometry'].exterior.coords]) 
+    vertices = extract_vertices(shape_file_path) 
     
     # Define a geometry object to define the polygon
     shape_geometry = [{'type': 'Polygon',
