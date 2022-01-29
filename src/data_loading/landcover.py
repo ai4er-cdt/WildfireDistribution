ead932ee0b3c1569797a17320501816e1d1301e6
Sofija Stefanovic <sof.stefanovic@gmail.com>
2022-01-29 16:56:02 +0000

    adding landcover dataset class.

---
+++ src/data_loading/landcover.py
@@ -0,0 +1,62 @@
+import torch
+from torchgeo.datasets.geo import RasterDataset
+from typing import Any, Callable, Dict, Optional
+
+from rasterio.crs import CRS
+
+
+class LandcoverSimple(RasterDataset):
+  filename_glob = "*Simple.tif"
+
+  def __init__(
+        self,
+        root: str = None,
+        crs: Optional[CRS] = None,
+        res: Optional[float] = None,
+        transforms: Optional[Callable[[Dict[str, Any]], Dict[str, Any]]] = None,
+        cache: bool = True,
+    ) -> None:
+        """Initialize a new Dataset instance.
+
+        Args:
+            root: root directory where dataset can be found
+            crs: :term:`coordinate reference system (CRS)` to warp to
+                (defaults to the CRS of the first file found)
+            res: resolution of the dataset in units of CRS
+                (defaults to the resolution of the first file found)
+            transforms: a function/transform that takes an input sample
+                and returns a transformed version
+            cache: if True, cache file handle to speed up repeated sampling
+        Raises:
+            FileNotFoundError: if no files are found in ``root``
+        """
+
+        super().__init__(root, crs, res, transforms, cache)
+
+class LandcoverComplex(RasterDataset):
+  filename_glob = "*Complex.tif"
+
+  def __init__(
+        self,
+        root: str = None,
+        crs: Optional[CRS] = None,
+        res: Optional[float] = None,
+        transforms: Optional[Callable[[Dict[str, Any]], Dict[str, Any]]] = None,
+        cache: bool = True,
+    ) -> None:
+        """Initialize a new Dataset instance.
+
+        Args:
+            root: root directory where dataset can be found
+            crs: :term:`coordinate reference system (CRS)` to warp to
+                (defaults to the CRS of the first file found)
+            res: resolution of the dataset in units of CRS
+                (defaults to the resolution of the first file found)
+            transforms: a function/transform that takes an input sample
+                and returns a transformed version
+            cache: if True, cache file handle to speed up repeated sampling
+        Raises:
+            FileNotFoundError: if no files are found in ``root``
+        """
+
+        super().__init__(root, crs, res, transforms, cache)
