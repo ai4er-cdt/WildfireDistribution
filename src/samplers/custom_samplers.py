41ae5e169947f08aababc4c63f4eafcefd685c35
Hamish Campbell <hrac2@cam.ac.uk>
2022-02-17 16:11:36 +0000

    Fixes batch grouping for custom sampler

--- src/samplers/custom_samplers.py
+++ src/samplers/custom_samplers.py
@@ -115,16 +115,14 @@ class ConstrainedRandomBatchGeoSampler(RandomBatchGeoSampler):
     """Returns batches of samples that meet the constraints specified:
         1. Proportion of samples with a burn proportion < 'burn_th': 'no_burn_prop'
         2. The rest of the samples will make up 1-'no_burn_prop' proportion and have a burn proportion > 'burn_th'
-
     Args:
         dataset: the dataset to take samples from.
         size: size of patch in lat/lon.
-        batch_size: the number of batches to take per epoch.
-        length: number of samples (in total) to take per epoch. Note: this means samples per batch = length/batch_size.
+        batch_size: the number of samples per batch.
+        length: number of samples (in total) to take per epoch. Note: this means no. of batches = length/batch_size.
         not_burned_prop: proportion of returned samples present that are "not burned". This may be no burn/some burn.
         burn_th: minimum threshold proportion required for a sample to be classed as "burned.
         roi: region of interest to take samples from.
-
     Returns:
         constrained_samples: set of samples that meet the specified constraints.
     """
@@ -147,7 +145,7 @@ class ConstrainedRandomBatchGeoSampler(RandomBatchGeoSampler):
         roi: Optional[BoundingBox] = None,
     ) -> None:
         
-        # Ensures that the input dataset is of type: Modis_JD
+        # Ensures that the input dataset is of type: Modis_JD
         if not isinstance(dataset, MODIS_JD):
             raise TypeError("Input dataset to sampler must be of type: MODIS_JD.")
 
@@ -160,72 +158,50 @@ class ConstrainedRandomBatchGeoSampler(RandomBatchGeoSampler):
         self.burn_th = burn_th
 
         # Set the number of samples required of not burned/burned types
-        # Note: len(self) = length/batch_size = number of samples per batch
-        self.not_burned_samples_required = math.floor(not_burned_prop * len(self))
-        self.burn_samples_required = len(self) - self.not_burned_samples_required
+        self.not_burned_samples_required = math.floor(not_burned_prop * self.batch_size)
+        self.burn_samples_required = self.batch_size - self.not_burned_samples_required
 
     def __iter__(self) -> Iterator[List[BoundingBox]]:
-        """Defines a generator function of sets of areas to sample next.
-           Note: sets are defined as a set of samples where one sample is assigned to each batch. I.e. need len(self) sets
-                 to complete all batches.
+        """Defines a generator function to produce batches of areas to sample next.
 
         Returns:
             List((minx, maxx, miny, maxy, mint, maxt)) coordinates to index a dataset
         """
 
-        # Generate sets of samples until all batches are complete
+        # Generate samples in len(self) = length/batch_size = number of batches required
         for _ in range(len(self)):
 
             # Choose a random tile
             hit = random.choice(self.hits)
             bounds = BoundingBox(*hit.bounds)
 
-            # Initialise an empty set
-            sample_set = []
+            # Fill a new batch of samples
+            batch = []
+            while self.not_burned_samples_required != 0 or self.burn_samples_required != 0:
+            
+                # Choose a random sample within that tile
+                bounding_box = get_random_bounding_box(bounds, self.size, self.res)
+                burn_prop = self.get_burn_proportion(bounding_box)
+                
+                # If we have a "not-burned" sample and we require "not-burned" samples
+                if burn_prop < self.burn_th and self.not_burned_samples_required != 0:
+                    self.not_burned_samples_required -= 1
+                    batch.append(bounding_box)
 
-            # Consider samples of the "not-burned" type if more are required
-            if self.not_burned_samples_required != 0:
+                # If we have a "burn" sample and we require "burn" samples
+                elif burn_prop >= self.burn_th and self.burn_samples_required != 0:
+                    self.burn_samples_required -= 1
+                    batch.append(bounding_box)
+            
+            # Return the batch of balanced samples we have gathered
+            yield batch
 
-                # Keep generating random bounding boxes until set is full
-                while len(sample_set) < self.batch_size:
-
-                    # Choose a random sample within that tile
-                    bounding_box = get_random_bounding_box(bounds, self.size, self.res)
-
-                    # Verify that "not-burned" constraint is satisfied
-                    burn_prop = self.get_burn_proportion(bounding_box)
-                    if burn_prop < self.burn_th:
-                        sample_set.append(bounding_box)
-
-                # Once the set is full, return the set
-                self.not_burned_samples_required -= 1
-                yield sample_set
-
-            # Consider samples of the "burn" type if more are required
-            elif self.burn_samples_required != 0:
-
-                # Keep generating random bounding boxes until set is full
-                while len(sample_set) < self.batch_size:
-
-                    # Choose a random sample within that tile
-                    bounding_box = get_random_bounding_box(bounds, self.size, self.res)
-
-                    # Verify that "burn" constraint is satisfied
-                    burn_prop = self.get_burn_proportion(bounding_box)
-                    if burn_prop >= self.burn_th:
-                        sample_set.append(bounding_box)
-
-                # Once the set is full, return the set
-                self.burn_samples_required -= 1
-                yield sample_set
-
-        # If all required samples generated, reset requirements for next call
-        self.not_burned_samples_required = math.floor(self.not_burned_prop * len(self))
-        self.burn_samples_required = len(self) - self.not_burned_samples_required
+            # Reset requirements for next batch generation
+            self.not_burned_samples_required = math.floor(self.not_burned_prop * self.batch_size)
+            self.burn_samples_required = self.batch_size - self.not_burned_samples_required
 
     def get_burn_proportion(self, bounding_box):
         """Returns the burn proportion found within a given bounding box.
-
         Returns:
             burn_prop: the burn proportion present within the bounding box.
         """
