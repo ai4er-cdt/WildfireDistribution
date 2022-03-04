5730acbe3101b389c9f038c7024d5304aa592dcc
Hamish Campbell <91635473+Hamish-Cam@users.noreply.github.com>
2022-03-04 17:04:35 +0000

    Changes not_burned_prop -> burn_prop

--- src/samplers/custom_samplers.py
+++ src/samplers/custom_samplers.py
@@ -10,14 +10,14 @@ from torchgeo.samplers.constants import Units
 
 class ConstrainedRandomBatchGeoSampler(RandomBatchGeoSampler):
     """Returns batches of samples that meet the constraints specified:
-        1. Proportion of samples with a burn proportion = 0 : 'no_burn_prop'
-        2. The rest of the samples will make up 1-'no_burn_prop' proportion and have a burn proportion > 0
+        1. Proportion of samples with a burn proportion > 0 : 'burn_prop'
+        2. The rest of the samples will make up 1-'burn_prop' proportion and have a sample burn proportion = 0
     Args:
         dataset: the dataset to take samples from.
         size: size of patch in lat/lon.
         batch_size: the number of samples per batch.
         length: number of samples (in total) to take per epoch. Note: this means no. of batches = length/batch_size.
-        not_burned_prop: proportion of returned samples present that are "not burned". This may be no burn/some burn.
+        burn_prop: proportion of returned samples present that are "burned". "burned" is defined as > 0 burned pixels.
         roi: region of interest to take samples from.
         units: Units.<PIXELS/CRS> depending on if patch size is in pixels or lat/lon
     Returns:
@@ -25,9 +25,9 @@ class ConstrainedRandomBatchGeoSampler(RandomBatchGeoSampler):
     """
 
     # Setup the attributes required for constraint implementation
-    not_burned_samples_required = None
     burn_samples_required = None
-    not_burned_prop = None
+    not_burned_samples_required = None
+    burn_prop = None
 
     # Define the constructor
     def __init__(
@@ -36,7 +36,7 @@ class ConstrainedRandomBatchGeoSampler(RandomBatchGeoSampler):
         size: Union[Tuple[float, float], float],
         batch_size: int,
         length: int,
-        not_burned_prop: float,
+        burn_prop: float,
         roi: Optional[BoundingBox] = None,
         units: Units = Units.PIXELS,
     ) -> None:
@@ -50,11 +50,11 @@ class ConstrainedRandomBatchGeoSampler(RandomBatchGeoSampler):
 
         # Save the dataset and input constraints to the object
         self.dataset = dataset
-        self.not_burned_prop = not_burned_prop
+        self.burn_prop = burn_prop
 
         # Set the number of samples required of not burned/burned types
-        self.not_burned_samples_required = math.floor(not_burned_prop * self.batch_size)
-        self.burn_samples_required = self.batch_size - self.not_burned_samples_required
+        self.burn_samples_required = math.ceil(burn_prop * self.batch_size)
+        self.not_burned_samples_required = self.batch_size - self.burn_samples_required
 
     def __iter__(self) -> Iterator[List[BoundingBox]]:
         """Defines a generator function to produce batches of areas to sample next.
@@ -76,20 +76,20 @@ class ConstrainedRandomBatchGeoSampler(RandomBatchGeoSampler):
             
                 # Choose a random sample within that tile
                 bounding_box = get_random_bounding_box(bounds, self.size, self.res)
-                burn_prop = self.get_burn_proportion(bounding_box)
+                samp_burn_prop = self.get_burn_proportion(bounding_box)
                 
                 # If we have a "not-burned" sample and we require "not-burned" samples
-                if burn_prop == 0 and self.not_burned_samples_required != 0:
+                if samp_burn_prop == 0 and self.not_burned_samples_required != 0:
                     self.not_burned_samples_required -= 1
                     batch.append(bounding_box)
 
                 # If we have a "burn" sample and we require "burn" samples
-                elif burn_prop > 0 and self.burn_samples_required != 0:
+                elif samp_burn_prop > 0 and self.burn_samples_required != 0:
                     self.burn_samples_required -= 1
                     batch.append(bounding_box)
 
                 # If we have found no "burn" samples so far, assume we need to change tile (speed)
-                elif self.burn_samples_required == (self.batch_size - math.floor(self.not_burned_prop*self.batch_size)):
+                elif self.burn_samples_required == math.ceil(burn_prop * self.batch_size):
                     hit = random.choice(self.hits)
                     bounds = BoundingBox(*hit.bounds)
             
@@ -97,8 +97,8 @@ class ConstrainedRandomBatchGeoSampler(RandomBatchGeoSampler):
             yield batch
 
             # Reset requirements for next batch generation
-            self.not_burned_samples_required = math.floor(self.not_burned_prop * self.batch_size)
-            self.burn_samples_required = self.batch_size - self.not_burned_samples_required
+            self.burn_samples_required = math.ceil(burn_prop * self.batch_size)
+            self.not_burned_samples_required = self.batch_size - self.burn_samples_required
 
     def get_burn_proportion(self, bounding_box):
         """Returns the burn proportion found within a given bounding box.
@@ -112,5 +112,5 @@ class ConstrainedRandomBatchGeoSampler(RandomBatchGeoSampler):
 
         # Get burn proportion within the bounding box
         non_zero_count = int((np.array(burn_data) > 0).sum())
-        burn_prop = non_zero_count / burn_data.numel()
-        return burn_prop
+        samp_burn_prop = non_zero_count / burn_data.numel()
+        return samp_burn_prop
