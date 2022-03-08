fbe814e821746cc79544611cbca163f186e11f4a
sofstef <sof.stefanovic@gmail.com>
2022-03-08 16:11:29 +0000

    adding sentinel to init

--- src/data_loading/__init__.py
+++ src/data_loading/__init__.py
@@ -1,8 +1,7 @@
 from .era5land import ERA5SnowC, ERA5SnowDepth, ERA5T2M, ERA5SWVL1
 from .modis_cci import MODIS_CCI, MODIS_JD
-<<<<<<< HEAD
 from .landsat import Landsat, Landsat7
->>>>>>> d0a6fda0daf33ac647b171dfd862017a63e9b489
+from .sentinel import Sentinel2
 
 # this is a bit of a clunky solution for now (for modis unzip)
 from .modis_unzip import unzip_all_modis_fire_files
@@ -17,7 +16,6 @@ __all__ = (
     "MODIS_CCI",
     "MODIS_JD",
     "unzip_all_modis_fire_files",
-<<<<<<< HEAD
     "Landsat7",
->>>>>>> d0a6fda0daf33ac647b171dfd862017a63e9b489
+    "Sentinel2",
 )
