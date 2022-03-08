7b3959ea4e574d56fbeda2238c3b0cab136dc360
graceebc9 <91670329+graceebc9@users.noreply.github.com>
2022-03-08 15:10:09 +0000

    update init to pull Landsat class

--- src/data_loading/__init__.py
+++ src/data_loading/__init__.py
@@ -1,5 +1,6 @@
 from .era5land import ERA5SnowC, ERA5SnowDepth, ERA5T2M, ERA5SWVL1
 from .modis_cci import MODIS_CCI, MODIS_JD
+from .landsat import Landsat7
 
 # this is a bit of a clunky solution for now (for modis unzip)
 from .modis_unzip import unzip_all_modis_fire_files
