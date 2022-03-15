4b8403eb4c080c78def6089ac1ecd17959610f40
Hamish Campbell <hrac2@cam.ac.uk>
2022-03-15 22:13:59 +0000

    Adds sample plotting callback functionality

--- src/evaluation/callbacks.py
+++ src/evaluation/callbacks.py
@@ -1,47 +1,35 @@
-from torchvision.utils import make_grid
-import torchvision.transforms as T
-
-### THIS IS STILL WIP, NEEDS MORE WORK BEFORE WE CAN GET EASY TO UNDERSTAND FIGURES WHILE TRAINING
+from pytorch_lightning.callbacks import Callback
+import wandb
 
 
 class LogPredictionsCallback(Callback):
+    
     def on_validation_batch_end(
-        self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx
-    ):
+        self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
         """Called when the validation batch ends."""
 
-        # `outputs` comes from `LightningModule.validation_step`
-        # which corresponds to our model predictions in this case
-
-        # Let's log 20 sample image predictions from first batch
+        # Dict of possible classes
+        class_labels = {
+              0: "no burn",
+              1: "burn",
+            }
+        
+        # Log the input, ground truth and prediction for the nth sample,
+        # once per epoch, on the first batch in epoch
+        n = 0
         if batch_idx == 0:
-            # print(batch)
-            n = 2
-            y = batch["mask"]
-            out = batch["prediction"]
-            # print(y)
-            # print(f"Type of y is:{type(y)}")
-            # print(out)
-            # print(f"Type of out is:{type(out)}")
-            # images = [img for img in x[:n]]
-            # captions = [f'Ground Truth: {y_i} - Prediction: {y_pred}' for y_i, y_pred in zip(y[:n], outputs[:n])]
+            orig_image = batch["image"][n]
+            ground_truth = batch["mask"][n].detach().numpy()
+            prediction_mask = batch["prediction"][n].detach().numpy()
 
-            # Option 1: log images with `WandbLogger.log_image`
-            gtruth = [img for img in y[:n]]
-            preds = [pred for pred in out[:n]]
-            print(len(gtruth))
-            print(len(preds))
-            image_array = make_grid(gtruth + preds)
-            # print(image_array)
-            print(f"Shape of torch image array is:{image_array.shape}")
-            image_array = [
-                T.ToPILImage()(image.to(torch.uint8).to("cpu")) for image in image_array
-            ]
-            # wandb_logger.log_image(key="sample_images", images=image_array, caption="TBA")
-            wandb.log({"examples": [wandb.Image(image) for image in image_array]})
-            # wandb_logger.log_image(key='sample_images', images=images, caption=captions)
-
-            # Option 2: log predictions as a Table
-            # columns = ['image', 'ground truth', 'prediction']
-            # data = [[wandb.Image(x_i), y_i, y_pred] for x_i, y_i, y_pred in list(zip(x[:n], y[:n], outputs[:n]))]
-            # wandb_logger.log_table(key='sample_table', columns=columns, data=data)
+            wandb.log(
+                {"my_image_key" : wandb.Image(orig_image, masks={
+                    "predictions" : {
+                        "mask_data" : prediction_mask,
+                        "class_labels" : class_labels
+                    },
+                    "ground_truth" : {
+                        "mask_data" : ground_truth,
+                        "class_labels" : class_labels
+                    }
+                })})
