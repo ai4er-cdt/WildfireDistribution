1fc153d2c12764c235ede97d5502e56b25e8d97c
graceebc9 <91670329+graceebc9@users.noreply.github.com>
2022-03-17 19:43:23 +0000

    update with notes

--- src/data_loading/sentinel.py
+++ src/data_loading/sentinel.py
@@ -5,6 +5,13 @@ from rasterio.crs import CRS
 from torchgeo.datasets import Sentinel2
 
 class Sentinel2(Sentinel2):
+    """
+    Custom Sentinel 2 Class
+     - works with new name and date format for monthly composite files. 
+     - Inherits properties from Raster Dataset  
+    
+    Note: Sampling will assume that all bands are present if the B03 is present: See the RasterDataset docs for more info
+    """
     filename_glob = '*B03.tif'
     filename_regex = '^(?P<date>\\d{6})\\S{4}(?P<band>B[018][\\dA]).tif$'
     date_format = '%Y%m'
