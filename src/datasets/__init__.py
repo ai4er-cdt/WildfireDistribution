25f860069c2e71e75e76c9b6e84087ecc6222e5b
graceebc9 <91670329+graceebc9@users.noreply.github.com>
2022-03-22 17:33:55 +0000

    remove mentions of landsat from src init

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,6 +1,5 @@
 from .era5land import ERA5SnowC, ERA5SnowDepth, ERA5T2M, ERA5SWVL1
 from .modis_cci import MODIS_CCI, MODIS_JD
-from .landsat import Landsat, Landsat7
 from .sentinel import Sentinel2
 
 # this is a bit of a clunky solution for now (for modis unzip)
@@ -16,7 +15,6 @@ __all__ = (
     "MODIS_CCI",
     "MODIS_JD",
     "unzip_all_modis_fire_files",
-    "Landsat7",
     "Sentinel2",
     "pull_monthly_cloudless_sentinel",
     "download_landsat",
