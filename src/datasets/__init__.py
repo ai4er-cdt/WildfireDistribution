d4f6aee546151d75b303701669d99121bbb69742
graceebc9 <91670329+graceebc9@users.noreply.github.com>
2022-03-23 10:54:04 +0000

    add landsat

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,6 +1,7 @@
 from .era5land import ERA5SnowC, ERA5SnowDepth, ERA5T2M, ERA5SWVL1
 from .modis_cci import MODIS_CCI, MODIS_JD
 from .sentinel import Sentinel2
+from .landsat import Landsat7
 
 # this is a bit of a clunky solution for now (for modis unzip)
 from .utils import unzip_all_modis_fire_files, pull_monthly_cloudless_sentinel, download_landsat
@@ -18,4 +19,5 @@ __all__ = (
     "Sentinel2",
     "pull_monthly_cloudless_sentinel",
     "download_landsat",
+    "Landsat7",
 )
